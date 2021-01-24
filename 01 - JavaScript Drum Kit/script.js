46e45007980fea17445893f7a052ee1a04e69630
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-01-24 21:13:32 +0300

    init: project initialization with snippets & complete Drum kit task

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,22 @@
+function playSound(e) {
+  const audio = document.querySelector(
+    `audio[data-key="${e.key.charCodeAt(0)}"]`
+  );
+
+  if (!audio) return;
+
+  audio.currentTime = 0; //Проиграть не дожидаясь конца текущего выполенения
+  audio.play();
+
+  const key = document.querySelector(`.key[data-key="${e.key.charCodeAt(0)}"]`);
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll(`.key`);
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
